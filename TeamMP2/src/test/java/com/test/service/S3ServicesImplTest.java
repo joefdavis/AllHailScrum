375ad07f8ad8304975603f5de7ce43153d80caee
John Kelly <JohnThaddeusKelly@outlook.com>
2019-11-07 14:22:08 -0500

    A few more tests

---
+++ TeamMP2/src/test/java/com/test/service/S3ServicesImplTest.java
@@ -0,0 +1,14 @@
+package com.test.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class S3ServicesImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
